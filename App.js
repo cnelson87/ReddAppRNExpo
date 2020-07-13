b9205221676cf6f265fde57a539a94adc4e46e05
Chris Nelson <chris.nelson@wearepop.com>
2020-07-13 14:51:08 -0700

    switch from yarn to npm

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { SafeAreaView, StyleSheet, View, Text } from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	body: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	section: {
+		marginVertical: 20,
+		paddingHorizontal: 20,
+	},
+	title: {
+		color: '#000',
+		fontSize: 24,
+		fontWeight: '600',
+		marginVertical: 5,
+	},
+	text: {
+		color: '#333',
+		fontSize: 18,
+		fontWeight: '400',
+		marginVertical: 5,
+	},
+	strong: {
+		fontWeight: '700',
+	},
 });
+
+function App() {
+	return (
+		<SafeAreaView style={styles.body}>
+			<StatusBar style="auto" />
+			<View style={styles.section}>
+				<Text style={styles.title}>React Native</Text>
+				<Text style={styles.text}>
+					Edit <Text style={styles.strong}>App.js</Text> to change this screen and then come back to see your edits.
+				</Text>
+			</View>
+		</SafeAreaView>
+	);
+}
+
+export default App;
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ReddAppRNExpo
+
+To run your project, navigate to the directory and run one of the following yarn commands.
+
+• cd "/path/to/project/ReddAppRN"
+• 'npm start' # you can open iOS, Android, or web from here, or run them directly with the commands below.
+• 'npm run ios'
+• 'npm run android'
+• 'npm run web'
